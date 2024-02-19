5b422f5247a0d92cd19f9b3d2b5f70fe531396e5
Martin Andreas Andersen <martin.andreas.andersen@gmail.com>
2024-02-19 23:29:26 +0100

    fix (kinda) the CV gen script
    For some reason, the for loop throws xelatex off. brute forced instead.
    Less elegant, sure. But works.

--- _data/cv_gen.sh
+++ _data/cv_gen.sh
@@ -1,14 +1,25 @@
+#!/bin/bash
 # rebuild CV and add it to /assets/pdf
 # TODO: Some information is in _config.yml
 #       Should probably trigger a build as well
 #       Or, move author data out of _config.yml
-LANGS='da en'
-
-    for LANG in $LANGS
-    do
-        pandoc -f markdown _config.yml _data/cv_${LANG}.yml _data/cv.yml \
-        --template=_data/cv.tex \
-        --pdf-engine=xelatex \
-        -o assets/pdf/cv_${LANG}.pdf
-        git add assets/pdf/cv_${LANG}.pdf
-    done
+# LANGS='da en'
+#
+#     for LANG in $LANGS
+#     do
+#         pandoc -f markdown _config.yml _data/cv_${LANG}.yml _data/cv.yml \
+#         --template=_data/cv.tex \
+#         --pdf-engine=xelatex  \
+#         -o assets/pdf/cv_${LANG}.pdf
+#         git add assets/pdf/cv_${LANG}.pdf
+#     done
+pandoc -f markdown _config.yml _data/cv_da.yml _data/cv.yml \
+--template=_data/cv.tex \
+--pdf-engine=xelatex  \
+-o assets/pdf/cv_da.pdf
+git add assets/pdf/cv_da.pdf
+pandoc -f markdown _config.yml _data/cv_en.yml _data/cv.yml \
+--template=_data/cv.tex \
+--pdf-engine=xelatex  \
+-o assets/pdf/cv_en.pdf
+git add assets/pdf/cv_en.pdf
